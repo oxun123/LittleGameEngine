5f90c4c7dc1b8a9edbef3eb64e4886e2be2515f6
oxun_123 <csharpoxun@gmail.com>
2025-09-17 23:52:47 +0500

    Clean code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 #include "first_app.h"
 
 int main() {
-    lve::FirstApp app;
-    lve::FirstApp app2;
-
     try {
+        lve::FirstApp app;
         app.run();
-        app2.run();
     } catch (const std::exception &e) {
         std::cerr << e.what() << "\n";
         return EXIT_FAILURE;
