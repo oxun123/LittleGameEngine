b5083230c6a7d324b061d4f25650504b587f3c62
oxun_123 <csharpoxun@gmail.com>
2025-09-28 17:52:19 +0500

    Lesson 10 2D multitransform

--- first_app.cpp
+++ first_app.cpp
@@ -24,7 +24,7 @@ namespace lve {
         createCommandBuffers();
     }
 
-    FirstApp::~FirstApp() { vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);}
+    FirstApp::~FirstApp() { vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr); }
 
     void FirstApp::run() {
         while (!lveWindow.shouldClose()) {
@@ -41,16 +41,27 @@ namespace lve {
             {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
             {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
         };
-
         auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
+        std::vector<glm::vec3> colors{
+            {1.f, .7f, .73f},
+            {1.f, .87f, .73f},
+            {1.f, 1.f, .73f},
+            {.73f, 1.f, .8f},
+            {.73, .88f, 1.f}
+        };
 
-        auto triangle = LveGameObject::createGameObject();
-        triangle.model = lveModel;
-        triangle.color = {.1f, .8f, .1f};
-        triangle.transform2d.translation.x = .2f;
-        triangle.transform2d.scale = {2.f, .5f};
-        triangle.transform2d.rotation = .25f * glm::two_pi<float>();
-        gameObjects.push_back(std::move(triangle));
+        for (auto &color: colors) {
+            color = glm::pow(color, glm::vec3(2.2f));
+        }
+
+        for (int i = 0; i < 40; i++) {
+            auto triangle = LveGameObject::createGameObject();
+            triangle.model = lveModel;
+            triangle.transform2d.scale = glm::vec2(.5f) + i * 0.025f;
+            triangle.transform2d.rotation = i * glm::pi<float>() * .025f;
+            triangle.color = colors[i % colors.size()];
+            gameObjects.push_back(std::move(triangle));
+        }
     }
 
 
@@ -64,7 +75,7 @@ namespace lve {
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount =  1;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
         pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
             throw std::runtime_error("failed to create pipeline layout!");
@@ -161,7 +172,7 @@ namespace lve {
         viewport.height = static_cast<float>(lveSwapChain->getSwapChainExtent().height);
         viewport.minDepth = 0.0f;
         viewport.maxDepth = 1.0f;
-        VkRect2D scissor {{0, 0}, lveSwapChain->getSwapChainExtent()};
+        VkRect2D scissor{{0, 0}, lveSwapChain->getSwapChainExtent()};
         vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
         vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
 
@@ -174,22 +185,25 @@ namespace lve {
     }
 
     void FirstApp::renderGameObjects(VkCommandBuffer commandBuffer) {
+        int i = 0;
+        for (auto &obj: gameObjects) {
+            i += 1;
+            obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.001f * i, 2.f * glm::two_pi<float>());
+        }
+
         lvePipeline->bind(commandBuffer);
-
-        for (auto& obj: gameObjects) {
-            obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
-
+        for (auto &obj : gameObjects) {
             SimplePushConstantData push{};
             push.offset = obj.transform2d.translation;
             push.color = obj.color;
             push.transform = obj.transform2d.mat2();
             vkCmdPushConstants(
-            commandBuffer,
-            pipelineLayout,
-            VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-            0,
-            sizeof(SimplePushConstantData),
-            &push);
+                commandBuffer,
+                pipelineLayout,
+                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                0,
+                sizeof(SimplePushConstantData),
+                &push);
             obj.model->bind(commandBuffer);
             obj.model->draw(commandBuffer);
         }
