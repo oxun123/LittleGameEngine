235de964c330c354a896e01848a7419776a52cbc
oxun123 <oxunjonsaidov8@gmail.com>
2025-10-13 12:13:12 +0500

    lesson16 Index and Staging buffer

--- lve_model.h
+++ lve_model.h
@@ -20,7 +20,12 @@ namespace lve {
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        LveModel(LveDevice &device, const std::vector<Vertex> &vertices);
+        struct Builder {
+            std::vector<Vertex> vertices;
+            std::vector<uint32_t> indices;
+        };
+
+        LveModel(LveDevice &device, const Builder &builder);
         ~LveModel();
 
         LveModel(const LveModel &) = delete;
@@ -31,11 +36,17 @@ namespace lve {
 
     private:
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffer(const std::vector<uint32_t> &indices);
 
         LveDevice &lveDevice;
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
         uint32_t vertexCount;
+
+        bool hasIndexBuffer = false;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
     };
 }
 
